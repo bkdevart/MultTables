9aae7ac3fccd75c2269ec0a869ccf1776f3a1026
Brandon Knox <bknox.dev@gmail.com>
2021-03-14 19:09:12 -0700

    Initial Commit

---
+++ MultTables/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MultTables
+//
+//  Created by Brandon Knox on 3/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MultTables/MultTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultTablesApp.swift
+//  MultTables
+//
+//  Created by Brandon Knox on 3/14/21.
+//
+
+import SwiftUI
+
+@main
+struct MultTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
