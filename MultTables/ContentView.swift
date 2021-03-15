1161546453eb870cafaca14e0f46da997d124fbc
Brandon Knox <bknox.dev@gmail.com>
2021-03-15 05:52:35 -0700

    created two views - settings and games.  state variable to switch between views.

--- MultTables/ContentView.swift
+++ MultTables/ContentView.swift
@@ -7,14 +7,56 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    
+    // settings view
+    struct Settings: View {
+        var text: String
+
+        var body: some View {
+            Text(text)
+                .font(.largeTitle)
+                .padding()
+                // .foregroundColor(.white)  // remove to apply custom to instances
+                .background(Color.blue)
+                .clipShape(Capsule())
+        }
+    }
+    
+    // game view
+    struct Game: View {
+        var text: String
+
+        var body: some View {
+            Text(text)
+                .font(.largeTitle)
+                .padding()
+                // .foregroundColor(.white)  // remove to apply custom to instances
+                .background(Color.blue)
+                .clipShape(Capsule())
+        }
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Group {
+            Settings(text: "Settings")
+                .foregroundColor(.white)
+            Game(text: "Game")
+                .foregroundColor(.white)
+            
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+    @State var gameActive = true
+    
+    
+    
+    
+    
     static var previews: some View {
         ContentView()
     }
